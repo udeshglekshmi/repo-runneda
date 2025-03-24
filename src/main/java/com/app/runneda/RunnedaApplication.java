30166329e9129d104215541618a6f78302ef29ee
udeshglekshmi <udesh4dev@gmail.com>
2025-03-24 07:40:44 +0530

    Add application properties for the project

---
+++ src/main/java/com/app/runneda/RunnedaApplication.java
@@ -0,0 +1,13 @@
+package com.app.runneda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnedaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/runneda/RunnedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.runneda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
